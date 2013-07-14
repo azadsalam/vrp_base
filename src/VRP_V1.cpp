10f0924d18dc4eb7bbc19f77a2edaec0530eb51b
unknown <DELL@DELL-PC.(none)>
2013-07-14 12:41:22 +0600

    fresh copy

---
+++ src/VRP_V1.cpp
@@ -0,0 +1,405 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+/*
+class ProblemInstance
+{
+	int ncustomers;
+	int ndepots;
+	int nnodes;
+	int nperiods;
+	int nvehicles;
+	int* vehicleAllocation; // HOW MANY VEHICLES PER DEPOT
+	int** cost;
+
+public:
+	ProblemInstance(int ncustomers, int ndepots, int nperiods, int nvehicles,
+			int* vehicleAllocation, int** cost)
+	{
+		this->ncustomers = ncustomers;
+		this->ndepots = ndepots;
+		this->nperiods = nperiods;
+		this->nvehicles = nvehicles;
+		this->nnodes = ndepots + ncustomers;
+
+		this->vehicleAllocation = new int[ndepots];
+		for (int i = 0; i < ndepots; i++)
+		{
+			this->vehicleAllocation[i] = vehicleAllocation[i];
+		}
+
+		this->cost = new int*[nnodes];
+
+		for (int i = 0; i < nnodes; i++)
+		{
+			this->cost[i] = new int[nnodes];
+		}
+
+		for (int i = 0; i < nnodes; i++)
+		{
+			for (int j = 0; j < nnodes; j++)
+			{
+				this->cost[i][j] = cost[i][j];
+			}
+		}
+	}
+
+};
+
+*/
+
+#define POPULATION_SIZE 3
+#define NUMBER_OF_GENERATION 5
+
+int testCases ;
+ifstream in ("input.txt");
+
+
+void escapeComment()
+{
+	char comment[255];
+	in.getline(comment,255);
+
+}
+
+class ProblemInstance
+{
+public:
+	int depotCount,customerCount,periodCount;
+	int* vehicleAllocation;
+	double **costMatrix;
+	int nodeCount;
+	int *frequencyAllocation;
+	int vehicleCount;
+
+
+void print()
+{
+	cout<<"Period : "<<periodCount<<endl;
+	cout<< "Number of depots : " <<depotCount <<endl;
+
+
+	cout<<"Number of vehicles : "<< vehicleCount <<" Allocation : ";
+	for(int j=0;j<depotCount;j++) cout<<vehicleAllocation[j]<<" ";
+	cout<<endl;
+
+	cout<<"Clients : "<<customerCount<<endl;
+
+	cout << "Frequency allocation : ";
+	for(int i =0;i<customerCount ;i++) cout << frequencyAllocation[i] << " ";
+	cout <<endl;
+
+	cout<< "Printing cost matrix : \n";
+
+	for(int row=0;row<nodeCount;row++)
+	{
+		for(int col=0;col<nodeCount;col++)
+			cout<<costMatrix[row][col]<<" ";
+		cout<<endl;
+
+	}
+	cout<<endl;
+}
+
+};
+
+class Individual
+{
+public:
+	bool** periodAssignment;
+	int** permutation;
+	int* routePartition;
+	double fitness;
+	bool isFeasible;
+	bool feasibilitySet;
+
+	ProblemInstance problemInstance;
+
+
+	Individual()
+	{
+		periodAssignment = NULL;
+		permutation = NULL;
+		routePartition = NULL;
+		fitness = -1;
+		feasibilitySet = false;
+		isFeasible = false;
+	}
+
+	void initialise(ProblemInstance problemInstance)
+	{
+		this->problemInstance = problemInstance;
+
+		// ALLOCATING periodCount * customerCount Matrix for Period Assignment
+		periodAssignment = new bool*[problemInstance.periodCount];
+		for(int i=0; i< problemInstance.periodCount; i++)
+		{
+			periodAssignment[i] = new bool[problemInstance.customerCount];
+
+			//initialise every period assignment with false
+			for(int j=0;j<problemInstance.customerCount;j++)
+				periodAssignment[i][j] = false;
+		}
+
+
+		//ALLOCATING permutation map matrix -> period * customer
+		permutation = new int*[problemInstance.periodCount];
+		for(int i=0;i<problemInstance.periodCount;i++)
+		{
+			permutation[i] = new int[problemInstance.customerCount];
+
+			// initially every permutation is identity permutation
+			for(int j=0;j<problemInstance.customerCount;j++)
+			{
+				permutation[i][j] = j;
+			}
+		}
+
+		//allocating routeAllocation
+		routePartition = new int[problemInstance.vehicleCount];
+
+
+		// NOW INITIALISE WITH VALUES
+
+		//initialize period assignment
+
+		int freq,allocated,random,tmp,j;
+
+		//Randomly allocate period to clients equal to their frequencies
+		for(int client=0; client < problemInstance.customerCount; client++)
+		{
+			freq = problemInstance.frequencyAllocation[client];
+			allocated=0;
+
+			while(allocated!=freq)
+			{
+				random = rand() % problemInstance.periodCount;
+				if(periodAssignment[random][client]==false)
+				{
+					periodAssignment[random][client]=true;
+					allocated++;
+				}
+			}
+		}
+
+		//initialize permutation map - KNUTH SHUFFLE
+		for(int period=0; period < problemInstance.periodCount;period++)
+		{
+			//apply knuths shuffle
+			for(int i = problemInstance.customerCount -1 ;i>0 ;i-- )
+			{
+				j = rand() % i ;
+
+				if(i == j) continue;
+
+				tmp = permutation[period][i];
+				permutation[period][i] = permutation[period][j];
+				permutation[period][j] = tmp;
+			}
+		}
+
+
+		//NEED TO GENERATE #vehicle-1 distinct random numbers in increasing order from [0,#customer - 1]
+		// DEVICE some faster and smarter algorithm
+
+		// route for vehicle i is  [ routePartition[i-1]+1 , routePartition[i] ]
+		// given that routePartition[i-1]+1 <= routePartition[i]
+
+		bool found;
+		allocated = 0;
+		while(allocated != problemInstance.vehicleCount-1)
+		{
+			random = rand() % problemInstance.customerCount;
+
+			found = binarySearch(random,allocated-1);
+
+			if(found) continue;
+			else
+			{
+				routePartition[allocated]=random;
+				sort(random,allocated);
+				allocated++;
+			}
+		}
+		routePartition[problemInstance.vehicleCount-1] = problemInstance.customerCount-1;
+
+	}
+
+	// sorts the array routePartition in increasing order
+	// input -> routePartition array [0, upperbound ], with,n inserted at the last in the array
+	// output -> sorted array [0, upperbound]
+	void sort(int n,int upperbound)
+	{
+		int tmp;
+		for(int i = upperbound-1;i>=0;i--)
+		{
+			if(routePartition[i]>routePartition[i+1])
+			{
+				tmp = routePartition[i];
+				routePartition[i] = routePartition[i+1];
+				routePartition[i+1] = tmp;
+			}
+			else
+				break;
+		}
+	}
+	//Searches if the number already exists or not, within the given range [0,upperBound] of routePartition
+	bool binarySearch(int n, int upperBound)
+	{
+		if(upperBound<0) return false;
+
+		int low=0;
+		int high=upperBound;
+		int mid;
+
+		while(low<=high)
+		{
+			mid = (high+low)/2;
+
+			if(routePartition[mid]==n) return true;
+			else if(n < routePartition[mid])
+				high = mid-1;
+			else
+				low = mid+1;
+		}
+
+		return false;
+	}
+
+	void print()
+	{
+		cout << "PERIOD ASSIGMENT : \n";
+		for(int i=0;i<problemInstance.periodCount;i++)
+		{
+			for(int j=0;j<problemInstance.customerCount;j++)
+			{
+				cout<<periodAssignment[i][j]<<" ";
+			}
+			cout << endl;
+		}
+
+		cout << "Permutation : \n";
+		for(int i=0; i<problemInstance.periodCount;i++)
+		{
+			for(int j=0;j<problemInstance.customerCount;j++)
+			{
+				cout << permutation[i][j]<<" ";
+			}
+			cout << endl;
+		}
+
+		cout<< "Route partition : ";
+		for(int i=0;i<problemInstance.vehicleCount;i++)cout<< routePartition[i] <<" ";
+		cout << endl;
+	}
+};
+
+class GeneticAlgo
+{
+	ProblemInstance problemInstance;
+	Individual population[POPULATION_SIZE];
+
+public:
+	GeneticAlgo(ProblemInstance problemInstance)
+	{
+		this -> problemInstance = problemInstance;
+	}
+	void run()
+	{
+		//problemInstance.print();
+
+		// INITIALISE POPULATION
+		initialisePopulation();
+
+		for(int generation=0;generation<NUMBER_OF_GENERATION;generation++)
+		{
+
+		}
+	}
+
+	void initialisePopulation()
+	{
+		for(int i=0; i<POPULATION_SIZE; i++)
+		{
+			population[i].initialise(problemInstance);
+			population[i].print();
+		}
+	}
+};
+
+
+void parseInputFile()
+{
+
+	cout << "OPENING INPUT FILE" << endl;
+	if(!in)
+	{
+		cout << "ERROR OPENING FILE!! EXIT !!"<< endl;
+		exit(1);
+	}
+
+
+	in>>testCases;
+	cout<<"TEST CASES : "<<testCases<<endl;
+	escapeComment();
+
+	for(int t=0 ; t<testCases;t++)
+	{
+		ProblemInstance problemInstance;
+
+		cout<< "TEST CASE "<<t+1<<" : "<<endl<<endl;
+
+		in>>problemInstance.periodCount;
+		escapeComment();
+
+		in>>problemInstance.depotCount;
+		escapeComment();
+
+		problemInstance.vehicleAllocation = new int[problemInstance.depotCount];
+		problemInstance.vehicleCount = 0;
+		for(int j=0;j<problemInstance.depotCount;j++)
+		{
+			in>>problemInstance.vehicleAllocation[j];
+			problemInstance.vehicleCount += problemInstance.vehicleAllocation[j];
+		}
+		escapeComment();
+
+		in>>problemInstance.customerCount;
+		escapeComment();
+
+		problemInstance.frequencyAllocation = new int[problemInstance.customerCount];
+
+		for(int i=0 ; i<problemInstance.customerCount; i++) in>> problemInstance.frequencyAllocation[i];
+		escapeComment();
+
+		problemInstance.nodeCount = problemInstance.customerCount+problemInstance.depotCount;
+
+		problemInstance.costMatrix = new double*[problemInstance.nodeCount];
+		for(int i=0;i<problemInstance.nodeCount;i++)problemInstance.costMatrix[i] = new double[problemInstance.nodeCount];
+
+
+		for(int row=0;row<problemInstance.nodeCount;row++)
+			for(int col=0;col<problemInstance.nodeCount;col++)
+				in>>problemInstance.costMatrix[row][col];
+
+
+		GeneticAlgo ga(problemInstance);
+		ga.run();
+	}
+	in.close();
+}
+
+
+
+
+
+int main()
+{
+	srand (time(NULL));
+	cout << "VRP CPP V1" << endl;
+	parseInputFile();
+
+	return 0;
+}
